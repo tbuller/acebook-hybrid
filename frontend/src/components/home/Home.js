44bd85c40d2347df9a2da3a9cd2cdd6f407ac9ec
Henry B <boylanh3nry@gmail.com>
2022-12-07 14:52:40 +0000

    .jpeg breaking background, commited before pulling

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from "react";
-import styles from "./Home.css"
+import styles from "./Home.css";
 
-const Home = ({navigate}) => {    
+const Home = ({ navigate }) => {
   return (
     <>
-    <head>
-    </head>
-    <div>
-      <h1>Hello there!</h1>
-    </div>
-    <div>
-        <body>
-            <p>Select below where you'd like to go</p>
-            <a href="/login"><button>Login</button></a>
-            <a href="/signup"><button>Sign up</button></a>
+      <head>
+      </head>
+      <div>
+        <h1>Welcome to the Mineshaft!</h1>
+      </div>
+      <div>
+        <body className={styles.background}>
+          <p>Select below where you'd like to go</p>
+          <a href="/login">
+            <button>Login</button>
+          </a>
+          <a href="/signup">
+            <button>Sign up</button>
+          </a>
         </body>
-    </div>
+      </div>
     </>
   );
-}
-
-
+};
 
 export default Home;
