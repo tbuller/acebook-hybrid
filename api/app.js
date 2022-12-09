4084754460dbd67f3a7f81643c5e365e0d5dbfd8
William Lines <williamlines42@gmail.com>
2022-12-09 09:05:00 +0000

    .

--- api/app.js
+++ api/app.js
@@ -36,10 +36,7 @@ const tokenChecker = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
+    res.setHeader("Access-Control-Allow-Headers", "*");
   }
   let token;
   const authHeader = req.get("Authorization");
